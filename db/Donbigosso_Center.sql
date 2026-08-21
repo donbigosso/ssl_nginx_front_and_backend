80dff38efba74304ac440a88ca515401232067a3
Bigos Malina <w.bogdan@ymail.com>
2026-08-21 12:59:32 +0200

    DB schema adjusted to include the messages table

--- db/Donbigosso_Center.sql
+++ db/Donbigosso_Center.sql
@@ -1,88 +1,145 @@
-CREATE TABLE `posts` (
-  `post_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `author_id` int(11) NOT NULL,
-  `topic` varchar(255) DEFAULT null,
-  `content` text DEFAULT null,
-  `date_added` datetime DEFAULT (current_timestamp())
-);
 
-CREATE TABLE `media_collections` (
-  `media_collection_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `title` varchar(200) DEFAULT null,
-  `description` varchar(255) DEFAULT null,
-  `register_date` datetime DEFAULT (current_timestamp())
-);
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `collection_owners`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `collection_owners` (
-  `user_id` int(11) NOT NULL,
-  `media_collection_id` int(11) NOT NULL,
-  `access_granted` datetime DEFAULT (current_timestamp()),
-  PRIMARY KEY (`user_id`, `media_collection_id`)
-);
-
-CREATE TABLE `media_items` (
-  `media_item_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `media_type` ENUM('PIC', 'VID', 'YT') NOT NULL,
-  `file_id` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT null,
-  `descr` text DEFAULT null,
-  `tags` text DEFAULT null,
-  `coordinates` longtext DEFAULT null
-);
-
+  `user_id` int NOT NULL,
+  `media_collection_id` int NOT NULL,
+  `access_granted` datetime DEFAULT (now()),
+  PRIMARY KEY (`user_id`,`media_collection_id`),
+  KEY `user_id` (`user_id`),
+  KEY `media_collection_id` (`media_collection_id`),
+  CONSTRAINT `collection_owners_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `collection_owners_ibfk_2` FOREIGN KEY (`media_collection_id`) REFERENCES `media_collections` (`media_collection_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `contact_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contact_messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `sender_ip` varchar(45) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `files` (
-  `file_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `date_added` datetime DEFAULT (current_timestamp()),
-  `size_in_kb` double DEFAULT null
-);
-
+  `file_id` int NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) DEFAULT NULL,
+  `date_added` datetime DEFAULT (now()),
+  `size_in_kb` double DEFAULT NULL,
+  PRIMARY KEY (`file_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=404 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `media_collections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `media_collections` (
+  `media_collection_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `register_date` datetime DEFAULT (now()),
+  `collection_cover_id` int DEFAULT NULL,
+  PRIMARY KEY (`media_collection_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `media_in_collection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `media_in_collection` (
-  `media_item_id` int(11) NOT NULL,
-  `media_collection_id` int(11) NOT NULL,
-  `date_added` datetime DEFAULT (current_timestamp()),
-  PRIMARY KEY (`media_item_id`, `media_collection_id`)
-);
-
+  `media_item_id` int NOT NULL,
+  `media_collection_id` int NOT NULL,
+  `date_added` datetime DEFAULT (now()),
+  PRIMARY KEY (`media_item_id`,`media_collection_id`),
+  KEY `media_item_id_idx` (`media_item_id`),
+  KEY `collection_id_idx` (`media_collection_id`),
+  CONSTRAINT `fk_mic_collection_id` FOREIGN KEY (`media_collection_id`) REFERENCES `media_collections` (`media_collection_id`),
+  CONSTRAINT `fk_mic_item_id` FOREIGN KEY (`media_item_id`) REFERENCES `media_items` (`media_item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `media_in_post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `media_in_post` (
-  `media_item_id` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL,
-  PRIMARY KEY (`media_item_id`, `post_id`)
-);
-
+  `media_item_id` int NOT NULL,
+  `post_id` int NOT NULL,
+  PRIMARY KEY (`media_item_id`,`post_id`),
+  KEY `fk_mip_post_id` (`post_id`),
+  CONSTRAINT `fk_mip_item_id` FOREIGN KEY (`media_item_id`) REFERENCES `media_items` (`media_item_id`),
+  CONSTRAINT `fk_mip_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `media_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `media_items` (
+  `media_item_id` int NOT NULL AUTO_INCREMENT,
+  `media_type` enum('PIC','VID','YT','AUD') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `file_id` int NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `descr` text,
+  `tags` text,
+  `coordinates` longtext,
+  `creation_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`media_item_id`),
+  KEY `media_file_ref` (`file_id`),
+  CONSTRAINT `media_file_ref` FOREIGN KEY (`file_id`) REFERENCES `files` (`file_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=404 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `post_id` int NOT NULL AUTO_INCREMENT,
+  `author_id` int NOT NULL,
+  `topic` varchar(255) DEFAULT NULL,
+  `content` text,
+  `date_added` datetime DEFAULT (now()),
+  PRIMARY KEY (`post_id`),
+  KEY `posts_index_0` (`author_id`),
+  CONSTRAINT `post_authors_ref` FOREIGN KEY (`author_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `user_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT null,
-  `password` varchar(255) DEFAULT null,
-  `recovery_question` varchar(255) DEFAULT null,
-  `recovery_answer` varchar(255) DEFAULT null,
-  `is_admin` tinyint(1) DEFAULT null,
-  `register_date` datetime DEFAULT null,
-  `token` varchar(255) DEFAULT null,
-  `token_validity` datetime DEFAULT null
-);
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `recovery_question` varchar(255) DEFAULT NULL,
+  `recovery_answer` varchar(255) DEFAULT NULL,
+  `is_admin` tinyint(1) DEFAULT NULL,
+  `register_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `token` varchar(255) DEFAULT NULL,
+  `token_validity` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-CREATE INDEX `posts_index_0` ON `posts` (`author_id`);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-CREATE INDEX `user_id` ON `collection_owners` (`user_id`);
-
-CREATE INDEX `media_collection_id` ON `collection_owners` (`media_collection_id`);
-
-CREATE INDEX `media_item_id_idx` ON `media_in_collection` (`media_item_id`);
-
-CREATE INDEX `collection_id_idx` ON `media_in_collection` (`media_collection_id`);
-
-CREATE UNIQUE INDEX `name_UNIQUE` ON `users` (`name`);
-
-ALTER TABLE `media_items` ADD CONSTRAINT `media_file_ref` FOREIGN KEY (`file_id`) REFERENCES `files` (`file_id`);
-
-ALTER TABLE `posts` ADD CONSTRAINT `post_authors_ref` FOREIGN KEY (`author_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `collection_owners` ADD CONSTRAINT `collection_owners_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `collection_owners` ADD CONSTRAINT `collection_owners_ibfk_2` FOREIGN KEY (`media_collection_id`) REFERENCES `media_collections` (`media_collection_id`);
-
-ALTER TABLE `media_in_collection` ADD CONSTRAINT `fk_mic_collection_id` FOREIGN KEY (`media_collection_id`) REFERENCES `media_collections` (`media_collection_id`);
-ALTER TABLE `media_in_collection` ADD CONSTRAINT `fk_mic_item_id` FOREIGN KEY (`media_item_id`) REFERENCES `media_items` (`media_item_id`);
-
-ALTER TABLE `media_in_post` ADD CONSTRAINT `fk_mip_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`);
-ALTER TABLE `media_in_post` ADD CONSTRAINT `fk_mip_item_id` FOREIGN KEY (`media_item_id`) REFERENCES `media_items` (`media_item_id`);
